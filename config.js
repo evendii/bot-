0a35ad8184a59a18e2005382ada3b00570b4ea50
evendii <107420960+evendii@users.noreply.github.com>
2022-06-15 01:12:02 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 let fs = require('fs')
 
-global.owner = ['621223918622'] // Put your number here
+global.owner = ['6289522183114'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json'))
 global.APIs = { // API Prefix
@@ -38,8 +38,8 @@ bearkey = 'https://pastelink.net/v20Md'
 ApiiKey = 'https://pastelink.net/v20Md'
 vinkey = 'vinko'
 
-namabot = 'Mikami Bot'
-namalu = 'Riyo Ofc'
+namabot = 'Micans Bot'
+namalu = 'Rohman Ofc'
 footer = '\n©King Of Bear'
 kasihcaption = `Nih Sayang`
 
